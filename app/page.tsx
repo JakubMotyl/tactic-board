c47a20edea785f9149de111093a5cdcd50aff597
Jakub Motyl <kubamotyl16@gmail.com>
2025-12-17 15:31:43 +0100

    create folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Hero } from "@/components/sections";
+import { Board } from "@/components/sections";
 
 export default function Home() {
   return (
-    <div className="text-cream">
-      <p>siedasniodaniosdaniodnioas</p>
-    </div>
+    <main>
+      <Hero />
+      <Board />
+    </main>
   );
 }
