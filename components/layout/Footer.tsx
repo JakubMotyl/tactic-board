a93bc59eb87d9706c3d0f499500bb1a606914d14
Jakub Motyl <kubamotyl16@gmail.com>
2026-01-07 00:34:28 +0100

    create footer section

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { FaGithub } from "react-icons/fa6";
 
 export default function Footer() {
-  return <div></div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="py-4 border-t border-cream/30 flex items-center justify-center">
+      <p className="text-sm text-cream/80 flex flex-col gap-2 items-center">
+        &copy; {currentYear} Tactic Board.
+        <a
+          href="https://github.com/JakubMotyl"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-cream hover:scale-105 duration-100"
+          aria-label="GitHub profile"
+        >
+          <FaGithub size={20} />
+        </a>
+      </p>
+    </footer>
+  );
 }
